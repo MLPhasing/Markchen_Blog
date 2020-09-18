7d9bb46434bdf92e7176195903cac17339d9035a
MarkChenYutian <markchenyutian@gmail.com>
2020-09-18 14:23:43 +0800

    button fix 2

--- ComputerScience3_Homework/Homework02.js
+++ ComputerScience3_Homework/Homework02.js
@@ -21,33 +21,35 @@ function isUserWin(user_result, program_result){
     return true;
 }
 
-scoreBoard = {
-    numGame : 0,
-    numUserWin : 0,
-    getWinRate : function getWinRate(){
-        return Math.round((this.numUserWin/this.numGame) * 100) + "%";
-    },
-    addGameRes : function addGameRes(userWin){
-        this.numGame += 1;
-        this.numUserWin += userWin ? 1 : 0;
+function mainProgram(){
+    scoreBoard = {
+        numGame : 0,
+        numUserWin : 0,
+        getWinRate : function getWinRate(){
+            return Math.round((this.numUserWin/this.numGame) * 100) + "%";
+        },
+        addGameRes : function addGameRes(userWin){
+            this.numGame += 1;
+            this.numUserWin += userWin ? 1 : 0;
+        }
     }
-}
 
-const status = ["paper", "scissor", "stone"];
-alert("Welcome to the Paper-scissor-stone game in the next century!")
-while (true){
-    let user_input = prompt("Which do you want to choose? Paper, Scissor, or the Stone?").toLowerCase();
-    user_status = convertStringToStatus(user_input, status);
-    if (user_status == -1){
-        alert("Seems that you didn't put a valid input here, try again please");
-        continue;
-    }
-    program_status = chooseStatusRandomly(status);
-    user_result = isUserWin(user_status, program_status);
-    if (user_result){alert("you win! You choose the "+status[user_status]+" and the program choose the "+status[program_status]);}
-    else{alert("you lose :( You choose the "+status[user_status]+" and the program choose the "+status[program_status]);}
-    scoreBoard.addGameRes(user_result);
-    if (confirm("Do you want to see your data statistic?")){
-        alert("You have played "+scoreBoard.numGame+", and your wining rate is"+scoreBoard.getWinRate())
+    const status = ["paper", "scissor", "stone"];
+    alert("Welcome to the Paper-scissor-stone game in the next century!")
+    while (true){
+        let user_input = prompt("Which do you want to choose? Paper, Scissor, or the Stone?").toLowerCase();
+        user_status = convertStringToStatus(user_input, status);
+        if (user_status == -1){
+            alert("Seems that you didn't put a valid input here, try again please");
+            continue;
+        }
+        program_status = chooseStatusRandomly(status);
+        user_result = isUserWin(user_status, program_status);
+        if (user_result){alert("you win! You choose the "+status[user_status]+" and the program choose the "+status[program_status]);}
+        else{alert("you lose :( You choose the "+status[user_status]+" and the program choose the "+status[program_status]);}
+        scoreBoard.addGameRes(user_result);
+        if (confirm("Do you want to see your data statistic?")){
+            alert("You have played "+scoreBoard.numGame+", and your wining rate is"+scoreBoard.getWinRate())
+        }
     }
 }
