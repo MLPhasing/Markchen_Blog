3bae0961eff4878cfbe2f0e31b42abf4287f56db
MarkChenYutian <markchenyutian@gmail.com>
2020-11-03 07:46:45 +0800

    Rebuild Project with React App

--- ComputerScience3_Homework/Practice1.js
+++
@@ -1,8 +0,0 @@
-function checkIsPrime(n, r, isFirst){
-    if (isFirst){n += 1;}
-    if (r == Math.ceil(Math.sqrt(n)) + 1){return n;}
-    if (n%r == 0){return checkIsPrime(n + 1, 2, false);}
-    else{return checkIsPrime(n, r+1, false)}
-}
-
-console.log(checkIsPrime(10989, 2, true));
